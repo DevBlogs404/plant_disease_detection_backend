6e484ee8ca0763d52299ae99c8b3665fadd5d55a
shubham <hoodashubham92@gmail.com>
2024-07-09 19:41:09 +0530

    final commit

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 //routes
 app.use("/api/v1", diseaseRoutes);
