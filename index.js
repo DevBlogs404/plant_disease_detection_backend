55543223175351e2897da43f8857a4d8f00dac0d
shubham <hoodashubham92@gmail.com>
2024-07-10 17:03:36 +0530

    updated index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://plant-disease-detection-frontend-eight.vercel.app/*",
+    origin: "https://plant-disease-detection-frontend-eight.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
